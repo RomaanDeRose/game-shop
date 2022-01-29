c12b64569d84c33f80e22e8915f8d6a7bb59b4ef
Roman De Rose <romandr37@gmail.com>
2022-01-29 19:54:11 -0300

    funcion onAdd y arreglo setSumProd()

--- src/components/itemCount.js
+++ src/components/itemCount.js
@@ -4,43 +4,43 @@ import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 import "./itemCount.css";
 
 const ItemCount = ({ stock }) => {
-	const [sumProd, setSumProd] = useState(0);
+  const [sumProd, setSumProd] = useState(0);
 
-	const addProd = () => {
-		if (sumProd < stock) {
-			setSumProd(sumProd + 1);
-		} else {
-			setSumProd(sumProd);
-		}
-	};
+  const addProd = () => {
+    if (sumProd < stock) {
+      setSumProd(sumProd + 1);
+    }
+  };
 
-	const removeProd = () => {
-		if (sumProd > 0) {
-			setSumProd(sumProd - 1);
-		} else {
-			setSumProd(sumProd);
-		}
-	};
+  const removeProd = () => {
+    if (sumProd > 0) {
+      setSumProd(sumProd - 1);
+    }
+  };
 
-	return (
-		<div className="itemCount-container">
-			<h3 className="itemCount__title">FIFA 22</h3>
-			<div className="itemCount-prodCant">
-				<FontAwesomeIcon
-					icon={faMinus}
-					className="itemCount__icon"
-					onClick={removeProd}
-				/>
-				<span>{sumProd}</span>
-				<FontAwesomeIcon
-					icon={faPlus}
-					className="itemCount__icon"
-					onClick={addProd}
-				/>
-			</div>
-			<button>Agregar al carrito</button>
-		</div>
-	);
+  const onAdd = () => {
+    console.log("Producto agregado!");
+  };
+
+  return (
+    <div className="itemCount-container">
+      <h3 className="itemCount__title">FIFA 22</h3>
+      <div className="itemCount-prodCant">
+        <FontAwesomeIcon
+          icon={faMinus}
+          className="itemCount__icon"
+          onClick={removeProd}
+        />
+        <span>{sumProd}</span>
+        <FontAwesomeIcon
+          icon={faPlus}
+          className="itemCount__icon"
+          onClick={addProd}
+        />
+      </div>
+      <button onClick={onAdd}>Agregar al carrito</button>
+    </div>
+  );
 };
 
 export default ItemCount;
