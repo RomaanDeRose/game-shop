44b85a6477ec9c3469482e6a4ac3474037b5a492
Roman De Rose <romandr37@gmail.com>
2022-03-04 03:42:55 -0300

    firebase Item Colecction II

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,28 +1,100 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { CartContext } from "../context/cartContext";
+import { collection, addDoc } from "firebase/firestore";
+import { DB } from "../firebase";
 import CartItem from "./cartItem";
 import "./cart.css";
 
 const Cart = () => {
   const { cart, clearCart, showTotal } = useContext(CartContext);
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [orderID, setOrderID] = useState(false);
+
+  const handleNameChange = (e) => setName(e.target.value);
+
+  const handlePhoneChange = (e) => setPhone(e.target.value);
+
+  const handleEmailChange = (e) => setEmail(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const totalPrice = showTotal();
+    if (![name, phone, email].some((text) => text.trim() === "")) {
+      const itemCollection = collection(DB, "orders");
+      const newItem = {
+        buyer: { name, phone, email },
+        items: [...cart],
+        date: new Date().toString(36),
+        total: totalPrice,
+      };
+      addDoc(itemCollection, newItem)
+        .then((item) => setOrderID(item.id))
+        .catch((err) => console.log(err));
+    }
+    clearCart();
+  };
 
   return (
     <div className="cart">
       <h2 className="cart-title">Carrito</h2>
       <div className="cart-container">
-        {cart.length === 0 ? (
+        {cart.length === 0 && orderID === false ? (
           <p className="noProducts">No hay productos en el carrito...</p>
         ) : (
           cart.map((item) => <CartItem key={item.id} item={item} />)
         )}
       </div>
-      {cart.length > 0 && (
-        <>
+      {cart.length > 0 && orderID === false && (
+        <div className="cart-order">
           <p className="cart-totalPrice">
             Total: <span>${showTotal()}</span>
           </p>
-          <button>Comprar</button>
+          {/* <button>Comprar</button> */}
           <button onClick={clearCart}>Borrar carrito</button>
+          <form className="form" onSubmit={handleSubmit}>
+            <h2>Completa tus datos!</h2>
+            <input
+              value={name}
+              type="text"
+              placeholder="Nombre..."
+              onChange={handleNameChange}
+              required
+            />
+            <input
+              value={phone}
+              type="number"
+              placeholder="Teléfono..."
+              onChange={handlePhoneChange}
+              required
+            />
+            <input
+              value={email}
+              type="email"
+              placeholder="Email..."
+              onChange={handleEmailChange}
+              required
+            />
+            <input type="submit" value="Comprar" />
+          </form>
+        </div>
+      )}
+      {orderID && (
+        <>
+          <p className="order-msg">
+            Compra existosa! Puedes seguir el envío con el siguiente ID:{" "}
+          </p>
+          <span className="order-msg--id">{orderID}</span>
+          <Link to="/">
+            <button>
+              <FontAwesomeIcon className="icon-home" icon={faHome} /> Volver al
+              Inicio
+            </button>
+          </Link>
         </>
       )}
     </div>
--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC3S6MXUuLUD4K8g3zqOFPFMWyaDz-F8Nk",
-  authDomain: "game-shop-918b9.firebaseapp.com",
-  projectId: "game-shop-918b9",
-  storageBucket: "game-shop-918b9.appspot.com",
-  messagingSenderId: "352776666595",
-  appId: "1:352776666595:web:ca8bb8f5fdf4d8648a74d6",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
